c07c6732a5588155141e361c9e4a1cc253aa864e
Anton <wrstrass@gmail.com>
2022-05-06 15:17:38 +0300

    Added GET/check and GET/users methods

--- server/index.js
+++ server/index.js
@@ -8,8 +8,9 @@ function saveData() {
     fs.writeFileSync("./data.json", JSON.stringify(data));
 }
 
-const host = "localhost";
-const port = 8080;
+const config = require("./config.json");
+const host = config.host;
+const port = config.port;
 
 const requestEmitter = new EventEmitter();
 requestEmitter.on("GET/register", (req, res, url) => {
@@ -19,6 +20,7 @@ requestEmitter.on("GET/register", (req, res, url) => {
             ip: url.searchParams.get("ip")
         };
         saveData();
+        console.log("User " + url.searchParams.get("username") + " was registred");
 
         res.writeHead(200);
     }
@@ -26,6 +28,22 @@ requestEmitter.on("GET/register", (req, res, url) => {
         res.writeHead(404);
     }
     res.end();
+});
+requestEmitter.on("GET/check", (req, res, url) => {
+    if (data[url.searchParams.get("username")] == undefined) {
+        res.writeHead(404);
+        res.end();
+    }
+    else {
+        res.setHeader("Content-Type", "application/json");
+        res.end(JSON.stringify({
+            ip: data[url.searchParams.get("username")].ip
+        }));
+    }
+});
+requestEmitter.on("GET/users", (req, res, url) => {
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(Object.keys(data)));
 });
 
 const requestListener = function (req, res) {
